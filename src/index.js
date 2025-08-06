53e10b5fe3bb9205d8019f75e368917e23f73eb1
Pablo Antônio De Oliveira <oliveiraapablo@gmail.com>
2025-08-06 13:46:19 -0300

    :zap: teste

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./infrastructure/http/routes/authRoutes.js";
 import siteRoutes from "./infrastructure/http/routes/siteRoutes.js";
 import "./infrastructure/scheduler/siteStatusScheduler.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/sites", siteRoutes);
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-
-// teste
